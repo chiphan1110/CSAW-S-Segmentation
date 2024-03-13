2f3e529c5cc1f859e4b79a0239d566fe5ee0d1d8
chiphan1110 <21chi.pth@vinuni.edu.vn>
2024-03-13 10:33:47 +0700

    Add data preprocessing

--- new-segment/prepare_data.py
+++ new-segment/prepare_data.py
@@ -12,34 +12,33 @@ import matplotlib.pyplot as plt
 
 def preprocess_segmentation_samples():
 
-    data_path = "/home/vishc1/hoang/CSAW-S/CsawS/anonymized_dataset"
-    save_path = "/home/vishc1/hoang/CSAW-S/CSAWS_preprocessed"
+    data_path = "/home/vishc1/hoang/CSAW-S/CsawS/anonymized_dataset/"
+    save_path = "/home/vishc1/hoang/CSAW-S/CSAWS_preprocessed/"
 
-    os.makedirs(save_path)
-
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+        
     classes = ["_pectoral_muscle", "_nipple"]
 
     img_size = 1024
     clahe = True
 
     for patient in tqdm(os.listdir(data_path)):
-        curr_path = data_path + patient + "/"
-        patient_save_path = save_path + patient + "/"
+        curr_path = data_path + patient + "/"             # /home/vishc1/hoang/CSAW-S/CsawS/anonymized_dataset/099/       
+        patient_save_path = save_path + patient + "/"     # /home/vishc1/hoang/CSAW-S/CSAWS_preprocessed/099/
         if not os.path.exists(patient_save_path):
             os.makedirs(patient_save_path)
-
-        print(curr_path)
         
-        for class_ in classes:
-            print(class_)
+        for class_ in classes:                              # _pectoral_muscle
             for file_ in os.listdir(curr_path):
-                # print(file_)
-                scan = file_.split(class_)[0]
-                scan_id = scan.split("_")[0]
-                # print(scan)
-                # print(scan_id)
-                
+                if class_ in file_:                         # 099_0_pectoral_muscle.png
+                    scan = file_.split(class_)[0]           # 099_0
+                    scan_id = scan.split("_")[0]            # 099
+            
+                    img = cv2.imread(curr_path + file_, 0)
+                    
 
+                
 def main():
     preprocess_segmentation_samples()
 
