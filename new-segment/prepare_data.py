a62278743447b56080f680281348253b0edbbd13
chiphan1110 <21chi.pth@vinuni.edu.vn>
2024-03-13 09:47:31 +0700

    Add data dir

--- new-segment/prepare_data.py
+++ new-segment/prepare_data.py
@@ -3,10 +3,14 @@ import os
 import tensorflow as tf
 from tensorflow.keras.layers.experimental import preprocessing
 import tensorflow_addons as tfa
+from utils import *
 import h5py
 import cv2
 import imutils
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
+def preprocess_segmentation_samples():
 
+    data_path = "/home/vishc1/hoang/CSAW-S/CsawS/anonymized_dataset"
+    save_path = "/home/vishc1/hoang/CSAW-S/CSAWS_preprocessed"
