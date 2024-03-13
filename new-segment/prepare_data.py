141dfa1d3cb10283c6d7e2b193c012b4db049c0b
chiphan1110 <chiphanphan03@gmail.com>
2024-03-13 10:47:53 +0800

    prepare

--- new-segment/prepare_data.py
+++ new-segment/prepare_data.py
@@ -1,12 +1,2 @@
-import numpy as np
-import os
-import tensorflow as tf
-from tensorflow.keras.layers.experimental import preprocessing
-import tensorflow_addons as tfa
-import h5py
-import cv2
-import imutils
-from tqdm import tqdm
-import matplotlib.pyplot as plt
 
 
